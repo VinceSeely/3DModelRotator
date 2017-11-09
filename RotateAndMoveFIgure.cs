589ec350c8f9347128d0a3b97634e85a128b4c9e
Vince Seely <vince@vinceseely.com>
2017-11-08 19:41:31 -0600

    set rotateAndMove to go in and out

--- RotateAndMoveFIgure.cs
+++ RotateAndMoveFIgure.cs
@@ -2,10 +2,36 @@
 {
    public class RotateAndMoveFIgure : MovePattern
    {
+      private int totalMoves;
+      private const int MAXMOVES = 20;
+      private bool moveOut;
+
+      public RotateAndMoveFIgure()
+      {
+         totalMoves = 0;
+         moveOut = true;
+      }
+
       public override void Move(Figure fig)
       {
-         fig.Translate(1f, 0, 0);
-         fig.Rotate(1f, 0, 0);
+         if (totalMoves > MAXMOVES)
+         {
+            totalMoves = 0;
+            moveOut = !moveOut;
+         }
+
+         if (moveOut)
+         {
+            fig.Translate(1f, 0, 0);
+            fig.Rotate(1f, 0, 0);
+            totalMoves++;
+         }
+         else
+         {
+            fig.Translate(-1, 0, 0);
+            fig.Rotate(-1, 0, 0);
+            totalMoves++;
+         }
       }
    }
 }
