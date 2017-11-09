d24f4639a1fc646bcfcb5160f546ad5209cb5fc7
Vince Seely <vince@vinceseely.com>
2017-11-08 19:56:08 -0600

    mad it so scale and rotate didnt make it disappear

--- ScaleAndRotate.cs
+++ ScaleAndRotate.cs
@@ -8,10 +8,33 @@ namespace Prog2
 {
    public class ScaleAndRotate : MovePattern
    {
+
+      private int totalMoves;
+      private const int MAXMOVES = 20;
+      private bool moveOpposite;
+
+      public ScaleAndRotate()
+      {
+         totalMoves = 0;
+         moveOpposite = true;
+      }
       public override void Move(Figure fig)
       {
-         fig.Rotate(1.0f, 1.0f, 0.0f);
-         fig.Scale(-1.0f, -1.0f, 0.0f);
+         if (totalMoves > MAXMOVES)
+         {
+            moveOpposite = !moveOpposite;
+         }
+         if (moveOpposite)
+         {
+            fig.Rotate(-1.0f, -1.0f, 0.0f);
+            fig.Scale(1.0f, 1.0f, 1.0f);
+         }
+         else
+         {
+            fig.Rotate(1.0f, 1.0f, 0.0f);
+            fig.Scale(-1.0f, -1.0f, -1.0f);
+         }
+         totalMoves++;
       }
    }
 }
