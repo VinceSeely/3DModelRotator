b3b3b0ce80a4a4dd3badac54e0bcc4aea122627a
vince Seely <vince@vinceseely.com>
2017-12-08 20:16:38 -0600

    prepparing to remove several move patterns

---
+++ DeprecatingMoves/MovePattern4.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog2
+{
+   public class MovePattern4 : MovePattern
+   {
+      private int totalMoves;
+      private const int MAXMOVES = 1000;
+      private bool moveOut;
+
+      public MovePattern4()
+      {
+         totalMoves = 1;
+         moveOut = true;
+      }
+
+      public override void Move(Figure fig)
+      {
+         if (totalMoves > MAXMOVES)
+         {
+            totalMoves = 1;
+            moveOut = !moveOut;
+         }
+
+         if (moveOut)
+         {
+            fig.Translate(0.01f, 0.1f, 0);
+            totalMoves++;
+         }
+         else
+         {
+            fig.Translate(-0.01f, -0.01f, 0);
+            totalMoves++;
+         }
+      }
+   }
+}
